382b59eff450367d9e2f44ab9669190d0f13a02c
MAHMUDHASANSADI <mahmudhasansadi92@gmail.com>
2026-03-28 10:05:33 +0600

    instence

--- oop/classes/Fruit.php
+++ oop/classes/Fruit.php
@@ -8,6 +8,13 @@ class Fruit {
     }
 }
 $fruit = new Fruit("apple", "red");
+var_dump($fruit instanceof Fruit);
+echo "<br>";
 echo "this is ".$fruit->name;
 echo "<br>";
 echo "color is " .$fruit->color;
+echo"<br>";
+$color = new Fruit("red", "blue");
+var_dump($color instanceof Fruit);
+echo "<br>";
+echo "color is " .$color->color;
